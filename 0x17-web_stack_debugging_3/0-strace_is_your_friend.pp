a51facb855570f3185c8786e28542409a610916c
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2023-08-25 11:50:02 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,10 @@
-# Puppet automated fix for wordpress
-exec { 'Debugging':
-  command  => 'sudo sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php && sudo service apache2 restart',
-  provider => shell,
+# A puppet manuscript to replace a line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
